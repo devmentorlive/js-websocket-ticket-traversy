e0fcaadc85f024dc8e9117aba6bded6f19fa04b1
Mark Tellez <mark.tellez@icloud.com>
2020-08-07 07:29:52 -0500

    done

--- client/script.js
+++ client/script.js
@@ -2,4 +2,15 @@ var ws = new WebSocket("ws://localhost:3001");
 
 ws.addEventListener("open", () => {
   console.log("connected to the server!");
+
+  ws.send(
+    JSON.stringify({
+      type: "buy-tickets",
+      payload: 1,
+    })
+  );
+});
+
+ws.addEventListener("message", ({ data }) => {
+  console.dir(data);
 });
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const ws = new require("ws");
 const wss = new ws.Server({ noServer: true });
 const clients = new Set();
 
+let availableTickets = 2;
+
 const server = http.createServer((req, res) => {
   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), handleConnection);
 });
@@ -10,6 +12,21 @@ const server = http.createServer((req, res) => {
 function handleConnection(sock) {
   clients.add(sock);
   console.log("client connected!");
+
+  sock.on("message", (e) => {
+    const json = JSON.parse(e);
+    switch (json.type) {
+      case "buy-tickets":
+        availableTickets = availableTickets - json.payload;
+        sock.send(
+          JSON.stringify({
+            type: "tickets-sold",
+            payload: json.payload,
+          })
+        );
+    }
+    console.log("tickets left:", availableTickets);
+  });
 
   sock.on("close", () => {
     console.log("client disconnected!");
